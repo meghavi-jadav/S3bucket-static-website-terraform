2115a4cade2668d564be76f17ee052e9c59ec3f6
Meghavi Jadav <mjadav@parkar.digital>
2025-05-07 18:00:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,15 +22,6 @@ resource "aws_s3_bucket" "static_website" {
 
 }
 
-resource "aws_s3_bucket_public_access_block" "policy" {
-  bucket = aws_s3_bucket.static_website.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
 resource "aws_s3_bucket_policy" "public_read" {
     bucket = aws_s3_bucket.static_website.id
     policy = data.aws_iam_policy_document.public_read.json
