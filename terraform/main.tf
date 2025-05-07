ff23d26bdcb339a27923294f17d7dfe48e19ec13
Meghavi Jadav <mjadav@parkar.digital>
2025-05-07 17:58:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
 
 provider "aws"{
     region = var.aws_region
@@ -20,6 +20,15 @@ resource "aws_s3_bucket" "static_website" {
 
     force_destroy = true
 
+}
+
+resource "aws_s3_bucket_public_access_block" "policy" {
+  bucket = aws_s3_bucket.static_website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "public_read" {
