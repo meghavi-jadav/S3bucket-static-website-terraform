48b8bb683cb3b553238750d92955d15355f833ae
Meghavi Jadav <mjadav@parkar.digital>
2025-05-07 15:30:23 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,36 @@
+provider "aws"{
+    region = var.aws_region
+}
+
+resource "aws_s3_bucket" "static_website" {
+    bucket = var.bucket_name
+
+    website {
+        index_document = "index.html"
+    }
+
+    force_destroy = true
+
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+    bucket = aws_s3_bucket.static_website.id
+    policy = data.aws_iam_policy_document.public_read.json
+}
+
+data "aws_iam_policy_document" "public_read" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.static_website.arn}/*"
+    ]
+  }
+}
