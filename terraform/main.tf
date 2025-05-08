ea5a46fdcb3862cc2dfc859d1da4b4e71d2583a3
Meghavi Jadav <mjadav@parkar.digital>
2025-05-08 11:49:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,20 +11,47 @@ provider "aws"{
     region = var.aws_region
 }
 
-resource "aws_s3_bucket" "static_website" {
+resource "aws_s3_bucket" "website_bucket" {
     bucket = var.bucket_name
 
-    website {
-        index_document = "index.html"
-    }
+    # website {
+    #     index_document = "index.html"
+    # }
 
-    force_destroy = true
+    # force_destroy = true
 
 }
 
-resource "aws_s3_bucket_policy" "public_read" {
-    bucket = aws_s3_bucket.static_website.id
-    policy = data.aws_iam_policy_document.public_read.json
+resource "aws_s3_bucket_policy" "Allow_public_read" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+
+  })
+  depends_on = [aws_s3_bucket_public_access_block.Allow_Public_Access]
+}
+
+resource "aws_s3_bucket_public_access_block" "Allow_Public_Access" {
+  bucket                  = aws_s3_bucket.website_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.id
+  index_document {
+    suffix = "index.html"
+  }
 }
 
 data "aws_iam_policy_document" "public_read" {
@@ -39,7 +66,7 @@ data "aws_iam_policy_document" "public_read" {
     ]
 
     resources = [
-      "${aws_s3_bucket.static_website.arn}/*"
+      "${aws_s3_bucket.website_bucket.arn}/*"
     ]
   }
 }
