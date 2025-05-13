df0c3829a7d9a7c773ca22ec10fa584299f9b65a
Meghavi Jadav <mjadav@parkar.digital>
2025-05-13 16:53:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,17 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
-
-provider "aws"{
-    region = var.aws_region
+provider "aws" {
+  region = var.aws_region
 }
 
 resource "aws_s3_bucket" "website_bucket" {
-    bucket = var.bucket_name
+  bucket = var.bucket_name
+}
 
-    # website {
-    #     index_document = "index.html"
-    # }
-
-    # force_destroy = true
-
+resource "aws_s3_bucket_public_access_block" "Allow_Public_Access" {
+  bucket                  = aws_s3_bucket.website_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_policy" "Allow_public_read" {
@@ -34,17 +26,8 @@ resource "aws_s3_bucket_policy" "Allow_public_read" {
         Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
       }
     ]
-
   })
   depends_on = [aws_s3_bucket_public_access_block.Allow_Public_Access]
-}
-
-resource "aws_s3_bucket_public_access_block" "Allow_Public_Access" {
-  bucket                  = aws_s3_bucket.website_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_website_configuration" "website_configuration" {
@@ -54,19 +37,77 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
   }
 }
 
-data "aws_iam_policy_document" "public_read" {
-  statement {
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
 
-    actions = [
-      "s3:GetObject",
-    ]
 
-    resources = [
-      "${aws_s3_bucket.website_bucket.arn}/*"
-    ]
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 5.0"
+#     }
+#   }
+# }
+
+# provider "aws"{
+#     region = var.aws_region
+# }
+
+# resource "aws_s3_bucket" "website_bucket" {
+#     bucket = var.bucket_name
+
+#     # website {
+#     #     index_document = "index.html"
+#     # }
+
+#     # force_destroy = true
+
+# }
+
+# resource "aws_s3_bucket_policy" "Allow_public_read" {
+#   bucket = aws_s3_bucket.website_bucket.id
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect    = "Allow",
+#         Principal = "*",
+#         Action    = "s3:GetObject",
+#         Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+#       }
+#     ]
+
+#   })
+#   depends_on = [aws_s3_bucket_public_access_block.Allow_Public_Access]
+# }
+
+# resource "aws_s3_bucket_public_access_block" "Allow_Public_Access" {
+#   bucket                  = aws_s3_bucket.website_bucket.id
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
+
+# resource "aws_s3_bucket_website_configuration" "website_configuration" {
+#   bucket = aws_s3_bucket.website_bucket.id
+#   index_document {
+#     suffix = "index.html"
+#   }
+# }
+
+# data "aws_iam_policy_document" "public_read" {
+#   statement {
+#     principals {
+#       type        = "*"
+#       identifiers = ["*"]
+#     }
+
+#     actions = [
+#       "s3:GetObject",
+#     ]
+
+#     resources = [
+#       "${aws_s3_bucket.website_bucket.arn}/*"
+#     ]
+#   }
+# }
